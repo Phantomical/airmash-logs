84d7f2205fa20375f8271f9c015d6f6cac303e62
Phantomical <phantomical@github.com>
2018-03-27 18:17:11 -0400

    Added statsbot source code

---
+++ statsbot/logger.js
@@ -0,0 +1,77 @@
+﻿
+const matchQuote = new RegExp('"', "g");
+const matchNewLine = new RegExp('\n', "g");
+const matchCR = new RegExp('\r', "g");
+
+function sanitizeString(str) {
+    str = str.replace(matchQuote, '\\"');
+    str = str.replace(matchNewLine, '\\n');
+    str = str.replace(matchCR, '\\r');
+    return str;
+}
+
+function getDateTime() {
+
+    var date = new Date();
+
+    var millisec = date.getMilliseconds();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec + ":" + millisec;
+
+}
+
+function makeMessage(type, obj) {
+    let str = "[" + type;
+
+    for (var prop in obj) {
+        if (typeof obj[prop] === 'string' || obj[prop] instanceof String) {
+            str += ', ' + prop + ': "' + sanitizeString("" + obj[prop]) + '"';
+        }
+        else if (obj[prop] instanceof Array) {
+            str += ', ' + prop + ': [' + obj[prop] + ']';
+        }
+        else {
+            str += ", " + prop + ": " + obj[prop];
+        }
+    }
+
+    return str + "]";
+}
+
+function log(type, obj) {
+    obj.time = getDateTime();
+    console.log(makeMessage(type, obj)); 
+}
+
+module.exports = {
+    debug_info: false,
+    active_info: true,
+    log: log,
+    debug: function (type, obj) {
+        if (module.exports.debug_info) {
+            log(type, obj);
+        }
+    },
+    optional: function (type, obj) {
+        if (module.exports.active_info) {
+            log(type, obj);
+        }
+    }
+};
