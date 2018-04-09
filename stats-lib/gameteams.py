0eaf880a0c375ab17e8caa0980d8383cc9e4a4af
Phantomical <phantomical@github.com>
2018-04-09 13:15:59 -0400

    Removed wrongly included file

--- stats-lib/gameteams.py
+++ stats-lib/gameteams.py
@@ -16,56 +16,6 @@ with open(sys.argv[1], 'r', errors='ignore') as file:
     names = dict()
 
     for entry in logparser.parse_log(contents):
-        if entry['record_type'] == 'PLAYER_NEW':
-            names[entry['id']] = entry['name']
-            teams[entry['team']-1].add(entry['id'])
-        elif entry['record_type'] == 'PLAYER_LEAVE':
-            for i in range(0, 1):
-                teams[i].discard(entry['id'])
-                names.pop(entry['id'])
-
-        elif entry['record_type'] == 'PLAYER_RETEAM':
-            for i in range(0, 1):
-                teams[i].discard(entry['id'])
-
-            teams[entry['team']-1].add(entry['id'])
-
-        elif entry['record_type'] == 'GAME_WIN':
-            print(teamnames[entry['team']] + " won,"
-                  + " bounty: " + entry['bounty']
-                  + " num players: " + 
-                 str(len(teams[0]) + len(teams[1])))
-            
-            print("red team:")
-            for member in teams[0]:
-                if member in names:
-                    print('\t' + names[member])
-
-            print("blue team:")
-            for member in teams[1]:
-                if member in names:
-                    print('\t' + names[member])
-
-            teams = (set(), set())
-
-                 
-        elif entry['record_type'] == 'SERVER_CUSTOM':
-            print(teamnames[int(entry['data'][5])-1] +
-                 " won, num players: " +
-                 str(len(teams[0]) + len(teams[1])))
-             
-            print("red team:")
-            for member in teams[0]:
-                if member in names:
-                    print('\t' + names[member])
-
-            print("blue team:")
-            for member in teams[1]:
-                if member in names:
-                    print('\t' + names[member])
-
-            teams = (set(), set())
-        elif entry['record_type'] == 'LOGIN':
-            teams = (set(), set())
-
+        if entry['record_type'] == 'LOGIN':
+            teams - 
 
