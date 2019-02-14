41ff21df2e88f9117a81bf6261e9ca2c1bf283fc
Phantomical <phantomical@github.com>
2019-02-13 20:04:54 -0500

    Update discord notification limit

--- statsbot/statsbot.js
+++ statsbot/statsbot.js
@@ -42,7 +42,7 @@ const modules = {
     rejoin: new GameEndLeaveModule(),
     afk: new AFKDetectorModule(4),
     scores: new ScoreTracker(),
-    discord: new DiscordNotifier(10)
+    discord: new DiscordNotifier(15)
 };
 const client = new AirmashClient(serverURL, true, {
     name: MYNAME,
